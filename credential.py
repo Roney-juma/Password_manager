2763245e5ead8025b8b8215e64a0ec8497c8534d
Roney-juma <ochirone10@gmail.com>
2021-06-01 19:48:23 +0300

    created objects to credential.py

--- credential.py
+++ credential.py
@@ -1,2 +1,25 @@
 import random
 import string
+import pyperclip
+
+
+class Credential:
+    """
+    Class that generates an instance of a Credential
+    """
+
+    def __init__(self, page_name, user_name, password):
+        '''
+        __init__ method that helps us define properties for our objects.
+        Args:
+            page_name:Name of page or account you want to save credentials for
+            user_name:Username of page you want to save
+            pass_word:Password of page you want to save
+        '''
+        self.page_name = page_name
+        self.user_name = user_name
+        self.password = password
+
+    list_account = []  # Empty credentials list
+
+    
