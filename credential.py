158d0da6360419e26da5ce8a8f3140ac0c902f37
Roney-juma <ochirone10@gmail.com>
2021-06-01 19:52:44 +0300

    added classmethods in credential.py

--- credential.py
+++ credential.py
@@ -34,4 +34,58 @@ class Credential:
         '''
         Credential.list_credentials.remove(self)
 
-    
+    @classmethod
+    def find_by_pagename(cls, pagename):
+        '''
+        Method that takes in a page name and returns a credential that matches that page name.
+        Args:
+            pagename: Page name to search for
+        Returns :
+            Credential that matches the pag ename.
+        '''
+        for credential in cls.credentials_list:
+            if credential.page_name == pagename:
+                return credential
+
+    @classmethod
+    def credential_exists(cls, pagename):
+        '''
+        Method that checks if a credential exists from the credential list.
+        Args:
+            pagename: Page name to search if it exists
+        Returns :
+            True or false depending if the credential exists
+        '''
+        for credential in cls.list_credentials:
+            if credential.page_name == pagename:
+                return True
+
+        return False
+
+    @classmethod
+    def display_credentials(cls):
+        '''
+        method that returns the credentials list
+        '''
+        return cls.credentials_list
+
+    # get random string password with letters, digits, and symbols
+
+    @classmethod
+    def generate_password(cls, length):
+        """
+        this method uses the string method to generate a password of random digits and letters
+        the length of the password is determined by the length passed in the function's parameter 
+        """
+        password_characters = string.ascii_letters + string.digits + string.punctuation
+        password = ''.join(random.choice(password_characters)
+                           for i in range(length))
+        return password
+
+    def copy_cred_password(self, pagename):
+        '''
+        method that copies credential password by pagename
+        '''
+        copy_cred = Credential.find_by_pagename(pagename)
+        pyperclip.copy(self.copy_cred.pass_word)
+
