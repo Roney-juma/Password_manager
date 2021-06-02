3bbff0ec77b10b22369e215f5c39a336777dadad
Roney-juma <ochirone10@gmail.com>
2021-06-02 12:48:09 +0300

    fuctions to the behaviour of Credentials

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 
-from account import Account
+from account import Account_details
 from credential import Credential
 import os
 import time
@@ -12,7 +12,7 @@ def create_account(username, fname, lname, p_word):
     '''
     Function to create new account
     '''
-    new_account = Account(username, fname, lname, p_word)
+    new_account = Account_details(username, fname, lname, p_word)
     return new_account
 
 
@@ -34,11 +34,76 @@ def check_account_exists(username):
     '''
     Function that check if an account with that username already exists and return a Boolean
     '''
-    return Account.account_exists(username)
+    return Account_details.account_exists(username)
 
 
 def auth_user(username, password):
     '''
     Function to authenicate user during login
     '''
-    return Account.auth_user(username, password)
+    return Account_details.auth_user(username, password)
+
+    def create_credential(page, username, password):
+        
+        '''
+        Function to create credentials
+        '''
+
+    new_credential = Credential(page, username, password)
+    return new_credential
+
+
+def save_credential(credential):
+    '''
+    Function to save credential
+    '''
+    credential.save_credential()
+
+
+def delete_credential(credential):
+    '''
+    Function to delete credential
+    '''
+    credential.delete_credential()
+
+
+def find_cred_by_pagename(pagename):
+    """
+    Function that finds a credential by pagename and returns the credentials
+    """
+    return Credential.find_by_pagename(pagename)
+
+
+def copy_cred_pass(pagename):
+    '''
+    Function to copy credential password            
+    '''
+    return Credential.copy_cred_password(pagename)
+
+
+def check_credential_exists(pagename):
+    '''
+    Function that check if a credential exists with that pagename and return a Boolean
+    '''
+    return Credential.credential_exists(pagename)
+
+
+def display_credentials():
+    '''
+    Function that returns all the saved credentials
+    '''
+    return Credential.display_credentials()
+
+
+def generate_password(length):
+    '''
+    Function that generte a random password
+    '''
+    return Credential.generate_password(length)
+
+
+def main():
+
+    login = False  # Set initial login value to false
+    sign_name = ''  # Name of user currently logged in
+    logged = True
